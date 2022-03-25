d9525d19d0a23a2f8849de6a6b9fb0a00bfde290
lilievabartha <60297696+lilievabartha@users.noreply.github.com>
2022-03-25 19:45:13 -0400

    lili summit 4

---
+++ Assets/VFX Graphs/TriggerScatter.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class TriggerScatter : MonoBehaviour
+{
+   
+   
+		public string floatParameterName;
+       
+        public VisualEffect yourVisualEffect;
+		
+  
+    void OnTriggerEnter(Collider other)
+    {
+       yourVisualEffect.SetFloat(floatParameterName, 0.62f);
+	   Debug.Log("entered");
+    }
+   
+}
