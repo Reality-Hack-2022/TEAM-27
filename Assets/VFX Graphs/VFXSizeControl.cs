7a6ace00c1594ce991106a8895845c855b1c2b08
lilievabartha <60297696+lilievabartha@users.noreply.github.com>
2022-03-25 10:15:03 -0400

    Lili commit
    Lili commit

---
+++ Assets/VFX Graphs/VFXSizeControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.VFX;
+
+
+public class VFXSizeControl : MonoBehaviour
+{
+	
+	public VisualEffect yourVisualEffect;
+	
+	private Slider vfx_size;
+	
+	public float size;
+	
+	
+	
+    void Start()
+    {
+        vfx_size = this.gameObject.GetComponent<Slider>();
+		
+    }
+
+    
+    public void SetSize()
+    {
+        size = vfx_size.value;
+		yourVisualEffect.SetFloat("Size over time", size);
+		
+    }
+}
